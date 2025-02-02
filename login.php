8f3e17f783b0fae4862c27b79c831baa3d8a1ffd
chk363 <gitchkip@aol.com>
2025-02-02 20:47:03 +0100

    added user authentication

--- login.php
+++ login.php
@@ -1,8 +1,35 @@
 <div class="min-vh-100">
-<?php require_once __DIR__. "/templates/header.php"; ?>
+<?php 
+    require_once __DIR__. "/templates/header.php";
+    require_once __DIR__. "/lib/pdo.php";
+    require_once __DIR__. "/lib/user.php";
+
+    $errors = [];
+
+    if (isset($_POST['loginUser'])) {
+        $user = verifyUserLoginPassword($pdo, $_POST['email'], $_POST['password']);
+
+        if ($user) {
+            //on va le connecter => session
+        } else {
+            //afficher une erreur
+            $errors[] = "E-mail ou mot de passe incorrect";
+        }
+    }
+
+?>
 
 <div class="container col-xxl-8 px-4 py-5 h-75">
     <h1>Se connecter</h1>
+
+    <?php 
+        foreach ($errors as $error) { ?>
+        <div class="alert alert-danger" role="alert">
+            <?=$error; ?>
+        </div>
+
+        <?php }
+    ?>
 
     <form action="" method="post">
         <div class="mb-3">
