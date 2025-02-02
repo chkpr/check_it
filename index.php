a0ea81d0fbf4a2848066a42f16eb1e187fe79b62
chk363 <gitchkip@aol.com>
2025-02-02 18:31:53 +0100

    added project homepage

--- index.php
+++ index.php
@@ -4,8 +4,111 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=², initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
 </head>
 <body>
-    Hello
+    <div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-5 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img height="50" src="./assets/images/logo-check-it.png">
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Accueil</a></li>
+        <li><a href="#" class="nav-link px-2">Nouvelle liste</a></li>
+        <li><a href="#" class="nav-link px-2">Mes listes</a></li>
+        <li><a href="#" class="nav-link px-2">À propos</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Connexion</button>
+        <button type="button" class="btn btn-primary">S'inscrire</button>
+      </div>
+    </header>
+  </div>
+
+<div class="container col-xxl-8 px-5 py-5 mt-5 container_bg">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="./assets/images/hero_img.jpg" class="d-block mx-lg-auto img-fluid rounded-4" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-secondary-emphasis lh-1 mb-3">Gardez vos listes<br>avec vous !</h1>
+        <p class="lead">Bienvenue sur CheckIt, votre nouvelle plateforme de création de listes de tâches en ligne. 
+            Avec CheckIt, vous pouvez facilement créer des listes de choses à faire pour tous les aspects de votre vie.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Créer une liste</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">À propos</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container text-center col-xxl-8 pt-5 pb-5">
+
+  <div class="row my-5 py-5 px-5 container_bg">
+    <h2 class="py-3">Découvrez les fonctionnalités principales :</h2>
+    <div class="col-md-4 my-2 py-3">
+        <div class="card w-100">
+            <div class="card-header">
+                <i class="bi bi-card-checklist"></i>
+            </div>
+  <div class="card-body">
+    <h3 class="card-title">Créez vos listes en nombre illimité</h3>
+       <a href="#" class="btn btn-primary">S'inscrire</a>
+  </div>
+</div>
+    </div>
+        <div class="col-md-4 my-2 py-3">
+        <div class="card w-100">
+            <div class="card-header">
+                <i class="bi bi-tags"></i>
+            </div>
+  <div class="card-body">
+    <h3 class="card-title">Classez vos listes par catégorie</h3>
+       <a href="#" class="btn btn-primary">S'inscrire</a>
+  </div>
+</div>
+    </div>
+        <div class="col-md-4 my-2 py-3">
+        <div class="card w-100">
+            <div class="card-header">
+                <i class="bi bi-search"></i>
+            </div>
+  <div class="card-body">
+    <h3 class="card-title">Retrouvez vos listes facilement</h3>
+       <a href="#" class="btn btn-primary">S'inscrire</a>
+  </div>
+</div>
+    </div>
+  </div>
+  </div>
+
+
+  
+
+  <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-body-secondary">© 2025 Check It</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <img height="50" src="./assets/images/logo-check-it.png">
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Nouvelle liste</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Mes listes</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">À propos</a></li>
+    </ul>
+  </footer>
+</div>
+  
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
