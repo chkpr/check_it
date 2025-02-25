b2dc0c25125885928103028fbc3dd278707ed0f6
chk363 <gitchkip@aol.com>
2025-02-25 19:45:06 +0100

    changed logo

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
     <p class="col-md-4 mb-0 text-body-secondary">© 2025 Check It</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <img height="50" src="./assets/images/logo-check-it.png">
+      <img height="40" src="./assets/images/logo-check-it.png">
     </a>
 
     <ul class="nav col-md-4 justify-content-end">
