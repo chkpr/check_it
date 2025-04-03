f57f18ab3892470245817ceec3fd5078c909c36c
chk363 <gitchkip@aol.com>
2025-04-03 18:28:00 +0200

    database connection fixes

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,4 +1,14 @@
 <?php
+
+$url = getenv('JAWSDB_MARIA_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+
 try
 {
     $pdo = new PDO("mysql:dbname=$database;host=$hostname;charset=utf8mb4", $username, $password);
