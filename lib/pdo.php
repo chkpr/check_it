5a73cd0ebda8b776338df87d44f4724604eed6e9
chk363 <gitchkip@aol.com>
2025-04-03 18:10:52 +0200

    fixed connection string to mysql

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $pdo = new PDO("mysql:dbname=$datablase;host=$hostname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql://gvf3ptewoxbqtg5o:cpfc459cwi9gqbaf@am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/d342ylm8hvvqu1ol");
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
