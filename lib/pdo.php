8842afa1bac304ebf825f36f3a4c25264b5d7068
chk363 <gitchkip@aol.com>
2025-04-03 18:24:45 +0200

    database connection fixes

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $pdo = new PDO("mysql:dbname=d342ylm8hvvqu1ol;host=am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8mb4", "gvf3ptewoxbqtg5o", "cpfc459cwi9gqbaf");
+    $pdo = new PDO("mysql:dbname=$database;host=$hostname;charset=utf8mb4", $username, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
