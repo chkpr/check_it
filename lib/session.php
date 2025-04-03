19c0f22026a55e750faa524322c748dc0d4bcce3
chk363 <gitchkip@aol.com>
2025-04-03 20:01:23 +0200

    modified session.php file

--- lib/session.php
+++ lib/session.php
@@ -3,7 +3,7 @@
 session_set_cookie_params([
     'lifetime' => 3600,
     'path' => '/',
-    
+    'domain' => 'https://glacial-temple-51496-87c24c489cfb.herokuapp.com',
     'httponly' => true
 ]);
 
